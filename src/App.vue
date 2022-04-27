41db1214bf7bf4c371e9e416fd7136a47fdcfba2
Sepehr-Aghdasi82 <Sepehr.Aghdasi82@gmail.com>
2022-04-27 12:06:41 +0430

    Add Remove Todo svg

--- src/App.vue
+++ src/App.vue
@@ -17,6 +17,7 @@
                         :key="index"
                         class="my-2 d-flex justify-content-start align-items-center"
                   >
+                  <div>
                         <label
                               @click="completedTodo(todo)"
                               :class="{ completed: todo.completed }"
@@ -31,7 +32,26 @@
                               />
                               {{ todo.content }}
                         </label>
-                        <button @click="removeTodo(todo)" class="ms-3">Remove task</button>
+                        </div>
+                        <div>
+                        <button @click="removeTodo(todo)" class="btn ms-3 text-danger">
+                              <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    width="26"
+                                    height="26"
+                                    fill="currentColor"
+                                    class="bi bi-x-circle"
+                                    viewBox="0 0 16 16"
+                              >
+                                    <path
+                                          d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"
+                                    />
+                                    <path
+                                          d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"
+                                    />
+                              </svg>
+                        </button>
+                        </div>
                   </li>
             </ul>
             <h4 v-if="todos.length === 0">Empty List</h4>
